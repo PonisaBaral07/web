9ba0e236c7178f639513ae1491673725ec924714
Sugam Gelal <sugamgelal@Sugams-MacBook-Air.local>
2024-02-04 10:08:29 +0545

    project

--- app/forgotpass/page.js
+++ app/forgotpass/page.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import Logo from "@/components/logo";
+import React from "react";
 
 const Forgotpass = () => {
   return (
-    <div>Forgotpass</div>
-  )
-}
+    <div className="flex justify-center mt-20 overflow-x-hidden">
+      <div className="shadow-2xl   w-[80%] md:w-[30%] rounded-lg p-4 h-[80vh] ">
+        <div className="flex justify-center">
+          <Logo />
+        </div>
 
-export default Forgotpass
+        <div className="ml-6 mt-8">
+          <div className="text-lg md:text-3xl font-extrabold  text-green-800">
+            {" "}
+            Find your Account
+          </div>
+          <div className="text-gray-400 text-justify mr-4 md:mr-20">
+            Enter email, phone number or username associated with the account to
+            chnage the password of the account
+          </div>
+          <input
+            type="text"
+            name=""
+            placeholder="Email, phone number or username "
+            className="w-full mt-8 bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+          />
+          <button
+            className="w-full mt-4 bg-green-800 text-white rounded p-2"
+            type="button"
+          >
+            Find My Account
+          </button>
+        </div>
+      </div>
+      <div className=""></div>
+    </div>
+  );
+};
+
+export default Forgotpass;
---
+++ app/not-found/page.js
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import React from "react";
+
+const Four = () => {
+  return (
+    <div className=" h-[100vh] flex items-center justify-center">
+
+<div className="flex flex-col  items-center  justify-center ">
+      <div className="felx flex-col  items-center text-center w-[50%] h-100vh justify-center">
+        <div className=" text-green-800 text-9xl font-extrabold">Oops!</div>
+        <div className="text-black font-bold mt-5 text-2xl">404-Page not found </div>
+        <div className="text-green-500">
+          The page you are looking for might have been removed had its name
+          changed or is temporarily unavailable.
+        </div>
+        <div className="flex justify-center mt-6">
+          <Link className="w-[50%]" href="/">
+          <div className="text-white bg-green-800 p-2  rounded-md cursor-pointer hover:bg-green-950 ">
+            Go to Home page
+          </div>
+          </Link>
+        </div>
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default Four;
