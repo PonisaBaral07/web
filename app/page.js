a0d058eb31b94cea7bf83b6d1468a8d14b8497e8
Sugam Gelal <sugamgelal@Sugams-MacBook-Air.local>
2024-02-07 16:21:03 +0545

    investordashboard

--- app/page.js
+++ app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
     <div>
       <Navbar/>
       <Hero/>
-      <About id ="about"/>
+      <div className=''><About id ="about"/></div>
       <Contact id = "contact"/>
       <Footer/>
     </div>
